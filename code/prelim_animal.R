2e54513775da5d4597c4243b6698aced8e51289c
fusillomia <mia.fusillo@utexas.edu>
2021-02-05 22:43:58 -0600

    Code for most common cat and dog names

--- code/prelim_animal.R
+++ code/prelim_animal.R
@@ -1,47 +1,79 @@
 ### First look at Austin Animal Data
 
 library(tidyverse)
+library(dplyr)
+library(plyr)
+library(ggplot2)
+library(lubridate)
 
 df <- read_csv("data/austin_animal.csv")
-
 head(df)
-as.data.frame(df[1:4,])
 
+as.data.frame(df[1:4,])
 str(df)
+table(df$"Animal Type")
+length(df$Name)
+count(df[!is.na(df$Name),])
+
 
 ### Playing around with names.....
+## get rid of *, NA, weights
 
 df$Name
+df$Name <- tolower(df$Name)
+df$Name <- gsub("\\*", "", df$Name) 
+df$Name <- gsub("[0-9]+", "", df$Name)
+df$Name <- gsub("grams", "", df$Name)
+df$Name
 
-# let's get rid of stars...
+## What is the most common cat name?
+## 46,367 cats
 
-gsub("\\*", "", df$Name)
+cats <- df %>% filter(`Animal Type` == "Cat")
+head(cats)
+nrow(cats)
 
-# work out how to remove anything with "Grams" in the name.
+count(cats, "Name") %>%
+  arrange(desc(freq)) %>%
+  head(20)
 
-
-# what's the most popular names?  (for cats, for dogs, by sex, by age ?)
-
-# Extend this code to find any animal with certain patterns:
-
-df$name <- gsub("\\*", "", df$Name)
-
-# let's make everything lowercase
-
-df$name <- tolower(df$name)
-df$name
-
-# search for a string match
-grepl("blue", df$name)
-
-df$name[grepl("blue", df$name)]
-
-df[grepl("blue", df$name),]
+## Most common names:
+## (1) Luna, 122
+## (2) Charlie, 119
+## (3) Lucy, 84
+## (4) Bella, 83
+## (5) Max, 83
+## (18) Mia, 60 
 
 
-###  Draw a graph (histogram/density type thing) 
-# showing what times of day animals get dropped off
-# do the same thing for days of the week (maybe this would be a bar graph?)
+## What is the most common dog name?
+## 70,184 dogs
+
+dogs <- df %>% filter(`Animal Type` == "Dog")
+head(dogs)
+nrow(dogs)  
+
+count(dogs, "Name") %>%
+  arrange(desc(freq)) %>%
+  head(20)
+
+## (1) Max, 538
+## (2) Bella, 472
+## (3) Luna, 413
+## (4) Rocky, 373
+## (5) Daisy, 362
+
+## Conclusion for cat and dog names:
+  ## Luna, Max, and Bella are both within the top 5 most common names for both cats and dogs
+
+
+
+## What is the most common day for animals to be dropped off?
+# bar graph
+
+## What is the most common time of day for animals to be dropped off?
+# histogram or density plot
+
 
 # some things to look up
 # lubridate package has functions to extract times and dates.
@@ -50,6 +82,18 @@ df[grepl("blue", df$name),]
 
 x <- df[1:3,3]  # just grab 3 dates and times.
 as.Date(x, format = "%m/%d/%Y %H:%M")
+
+df$DateTime
+df$Date <- sub()
+
+
+df$DateTime
+df$Time <- sub("^\\S+\\s+", '', df$DateTime)
+df$Time
+
+
+
+
 
 
 
@@ -60,6 +104,8 @@ as.Date(df$DateTime, format = "%m/%d/%Y %H:%M")
 lubridate::wday("2018-07-12")
 
 lubridate::wday(as.Date(df$DateTime, format = "%m/%d/%Y %H:%M"))
+
+
 
 # splitting info:
 

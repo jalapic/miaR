7afa09b85942dbe19711cbb67396926b319f3c7c
fusillomia <mia.fusillo@utexas.edu>
2021-04-12 12:28:46 -0500

    organizing miaR

---
+++ code/Shiny app.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+library(shiny)
+
+runExample("01_hello")
+
+## change this problematic data
+
+hts <- Heights
+
+ui <- fluidPage(
+  titlePanel("making a Shiny app"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "bins",
+                  label = "number of bins",
+                  min = 1,
+                  max = 50,
+                  value = 30)),
+    mainPanel(
+      plotOutput(outputId = "distPlot")))
+  )
+
+
+
+server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    x    <- hts$heights
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    hist(x, breaks = bins, col = "lightblue", border = "darkblue",
+         xlab = "heights (cm)",
+         ylab = "count",
+         main = "histogram of 100 female heights")})
+  }
+
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
+
+
